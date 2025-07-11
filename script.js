e17402689b83607d8d8a4f4311262a800f8a6c2e
Sivuyise Matwa <sivuyisematwa15@gmail.com>
2025-07-11 16:11:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+const form = document.getElementById('contact-form');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    fetch(form.action, {
+    method: form.method,
+    body: new FormData(form),
+    headers: { 'Accept': 'application/json' }
+    }).then(response => {
+    if (response.ok) {
+        alert('Thank you for your message! I will get back to you soon.');
+        form.reset();
+    } else {
+        alert('Oops! There was a problem submitting your form.');
+    }
+    }).catch(() => {
+    alert('Oops! There was a problem submitting your form.');
+    });
+});
